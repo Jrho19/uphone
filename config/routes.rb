3ca897e143abe4d8aadd2f9c6eb6d888c57378f2
John Rhoden <jrho19.jr@gmail.com>
2017-09-23 18:15:37 -0400

    setup controllers, routes for registration and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+  get '/login' => 'sessions#new'
+
+  post '/login' => 'sessions#create'
+
+  get '/logout' => 'sessions#destroy'
+
+  get '/register' => 'users#new'
+
+  resources :users, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
